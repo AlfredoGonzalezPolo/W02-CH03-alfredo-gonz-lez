0822faa95bf9e915366f7a4a4d67b0edbd01b618
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-05-03 23:04:44 +0200

    Reupdate principal files to fix errors

---
+++ app/counter.js
@@ -0,0 +1,49 @@
+const counterValue = document.querySelector(".output");
+let value = 0;
+
+const handleIncrementButton = () => {
+  value = Number(counterValue.textContent) + 1;
+  if (value >= 20) {
+    alert("Sorry, we can't count more :(");
+    value = 20;
+  }
+
+  counterValue.textContent = value;
+};
+
+const handleDecrementButton = () => {
+  value = Number(counterValue.textContent) - 1;
+  if (value < 0) {
+    value = 0;
+  }
+
+  counterValue.textContent = value;
+};
+
+const handleResetButton = () => {
+  value = Number(counterValue.textContent) - value;
+  counterValue.textContent = value;
+};
+
+const registerEventListeners = () => {
+  const incrementButton = document.querySelector(".increment-button");
+  incrementButton.addEventListener("click", () => {
+    handleIncrementButton();
+  });
+
+  const decrementButton = document.querySelector(".decrement-button");
+  decrementButton.addEventListener("click", () => {
+    handleDecrementButton();
+  });
+
+  const resetButton = document.querySelector(".reset-button");
+  resetButton.addEventListener("click", () => {
+    handleResetButton();
+  });
+};
+
+const playCounter = () => {
+  registerEventListeners();
+};
+
+playCounter();
